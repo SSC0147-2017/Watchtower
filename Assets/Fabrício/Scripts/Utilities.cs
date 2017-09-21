39640bd31e915a469def6bd6378106dc624df74f
Fabricio <fabricio-gf@hotmail.com>
2017-09-20 23:21:39 -0300

    Create options and credits panels with (incomplete) fade animation

---
+++ Assets/Fabrício/Scripts/Utilities.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Utilities : MonoBehaviour {
+
+
+    public IEnumerator FadeOut(GameObject screen, float fadeDelay)
+    {
+        screen.gameObject.SetActive(true);
+
+        Color c = screen.GetComponent<Image>().color;
+
+        float time = 0;
+
+        while (time < fadeDelay)
+        {
+            c.a = Mathf.Lerp(0.4f, 0f, time / fadeDelay);
+
+            screen.GetComponent<Image>().color = c;
+            yield return null;
+            time += Time.deltaTime;
+        }
+        screen.gameObject.SetActive(false);
+    }
+
+    public IEnumerator FadeIn(GameObject screen, float fadeDelay)
+    {
+        screen.gameObject.SetActive(true);
+
+        Color c = screen.GetComponent<Image>().color;
+
+        float time = 0;
+
+        while (time < fadeDelay)
+        {
+            c.a = Mathf.Lerp(0f, 0.4f, time / fadeDelay);
+
+            screen.GetComponent<Image>().color = c;
+            yield return null;
+            time += Time.deltaTime;
+        }
+
+    }
+}
