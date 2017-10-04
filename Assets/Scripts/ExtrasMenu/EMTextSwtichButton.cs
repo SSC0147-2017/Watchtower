ca28cdb6b3390dd9bbcfc770454845fd65172d9b
jvguima <jvguima@yahoo.com.br>
2017-10-04 17:58:12 -0300

    Began Extras Menus Funcionality + Text Assets
    +Buttons on the extrasMenu now change the loaded text

---
+++ Assets/Scripts/ExtrasMenu/EMTextSwtichButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/***
+ * EMTextSwitchButton = Extras Menu Text Switch Button
+ * Script usado pelos botões laterais para modificar 
+ * o TextAsset carregado pelo contentPanel
+ */
+
+public class EMTextSwtichButton : MonoBehaviour {
+
+	public TextAsset textAsset;
+	public EMContentPanel contentPanelScript;
+
+	private Button btn;
+
+	public void SwitchText(){
+		contentPanelScript.switchTextAsset (textAsset);
+	}
+
+}
