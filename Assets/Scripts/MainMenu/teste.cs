85a26aeafae587fd8dee312c3e7287c2ae3ae6c9
Fabricio <fabricio-gf@hotmail.com>
2017-10-04 23:30:47 -0300

    "Merge" AGORA VAI
    Peguei as cenas e assets q foram mudados do branch CharacterSelect e passei para cá.
    Não usar mais o branch CharacterSelect!

--- Assets/Scripts/MainMenu/teste.cs
+++ Assets/Scripts/MainMenu/teste.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class PanelBehaviour : MonoBehaviour
+public class teste : MonoBehaviour
 {
 
     public EventSystem EventSys;
