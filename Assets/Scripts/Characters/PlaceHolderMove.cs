bc5baacdd5e46b2f8da70fc96c5d83e28fb44001
jvguima <jvguima@yahoo.com.br>
2017-11-08 18:18:29 -0200

    First MAPGEN + first arena tests
    +Included the first MAPGEN package
    +Recooked the navmesh for the first level
    TODO: Improve collisions on the edges of walls

--- Assets/Scripts/Characters/PlaceHolderMove.cs
+++ Assets/Scripts/Characters/PlaceHolderMove.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlaceHolderMove : MonoBehaviour {
 
+	public float speed;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,8 +13,8 @@ public class PlaceHolderMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
-		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
 
 		transform.Translate(x, 0, z);
 
