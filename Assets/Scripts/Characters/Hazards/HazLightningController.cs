9d75d4f52eb9620af253ed29b6f0496da230b8f5
jvguima <jvguima@yahoo.com.br>
2017-11-15 09:23:37 -0200

    Began lightning Hazard

---
+++ Assets/Scripts/Characters/Hazards/HazLightningController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazLightningController : MonoBehaviour {
+
+	public Transform path;//Transform do gameobject com path
+	public float damage;
+
+	private Vector3[] waypoints;
+
+	void Start(){
+
+		waypoints = new Vector3[path.childCount];
+
+		for (int i = 0; i < path.childCount; i++) {
+			waypoints [i] = path.GetChild (i);
+		}
+
+	}
+
+	void OnTriggerStay (Collider col){
+		HealthController tgtHealth = col.gameObject.GetComponent<HealthController> ();
+		if (tgtHealth != null) {
+			tgtHealth.takeDamage (damage);
+		}
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
