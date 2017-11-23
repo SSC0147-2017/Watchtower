ad9b283374e5c7acac75b1d4a72daaaaab250808
jvguima <jvguima@yahoo.com.br>
2017-11-23 13:30:20 -0200

    Began Firehazard
    +Firehazard gameobject with particle system and lights that activates and deactivates with a given time
    
    TODO: Increase size of the particleSystems

---
+++ Assets/Scripts/Hazards/HazFireController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazFireController : MonoBehaviour {
+
+	public Collider damageZone;
+	public ParticlesOnOff fireObject; //Objeto com efeitos de particulas/iluminação, etc.
+	public float period; //Tempo em segundos entre ativações
+	public float activeTime;
+	public float damage; //Dano dado enquanto a entidade ficar no fogo
+
+	private bool isHazardActive;	//Estado booleano de fogo ativo ou não
+	private float currTimer;
+
+	// Use this for initialization
+	void Start () {
+		currTimer = period;
+		isHazardActive = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currTimer -= Time.deltaTime;
+
+		if (currTimer <= 0) {
+		
+			if (!isHazardActive)
+				ActivateHazard ();
+			else
+				DeactivateHazard ();
+		}
+	}
+
+	void ActivateHazard(){
+		currTimer = activeTime;
+		isHazardActive = true;
+		damageZone.enabled = true;
+		fireObject.ParticleSystemsPlay();
+	}
+
+	void DeactivateHazard(){
+		currTimer = period;
+		isHazardActive = false;
+		damageZone.enabled = false;
+		fireObject.ParticleSystemsStop();
+	}
+
+	void OnTriggerStay (Collider col){
+		HealthController tgtHealth = col.gameObject.GetComponent<HealthController> ();
+		if (tgtHealth != null) {
+			print ("dando dano");
+			tgtHealth.takeDamage (damage);
+		}
+	}
+}
---
+++ Assets/Scripts/ParticlesOnOff.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlesOnOff : MonoBehaviour {
+
+	//Vetor de todos os particleSystems
+	public ParticleSystem [] systems;
+	public Light light;
+
+	public void ParticleSystemsPlay(){
+		for (int i = 0; i < systems.Length; i++) {
+			systems [i].Play ();
+		}
+		if (light != null) {
+			light.enabled = true;
+		}
+	}
+
+	public void ParticleSystemsStop(){
+		for (int i = 0; i < systems.Length; i++) {
+			systems [i].Stop ();
+		}
+		if (light != null) {
+			light.enabled = false;
+		}
+	}
+}
