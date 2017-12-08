52ed03c2d1775f820abbd3f987d08c812cc5692b
Chieppe <chieppe2142@gmail.com>
2017-12-08 13:48:28 -0200

    Publish Commit

--- Assets/MapGenerator/MapGenEditor.cs
+++ Assets/MapGenerator/MapGenEditor.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿/*
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
@@ -17,3 +18,4 @@ public class MapGenEditor : Editor {
         }
     }
 }
+*/
