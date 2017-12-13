382f3d0dfd94188b33efcd01549658c219c9edf4
Fabricio <fabricio-gf@hotmail.com>
2017-12-13 12:06:06 -0200

    Added ambience sounds and floofy sounds rolloff

---
+++ Assets/Scripts/Sound/AmabienceManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmabienceManager : MonoBehaviour {
+
+	public AudioClip [] clips;
+	public AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+		PlayAmbienceClip ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!source.isPlaying) {
+			PlayAmbienceClip ();
+		}
+	}
+
+	void PlayAmbienceClip(){
+		int index = Random.Range(0, clips.Length);
+		source.PlayOneShot(clips[index], 0.5f);
+	}
+}
