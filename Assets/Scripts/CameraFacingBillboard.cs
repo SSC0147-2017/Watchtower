7a31184b3c70de054b038790fe76adddb4b5add9
Fabricio <fabricio-gf@hotmail.com>
2017-12-14 11:01:41 -0200

    Added revive progress bar

---
+++ Assets/Scripts/CameraFacingBillboard.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+ 
+public class CameraFacingBillboard : MonoBehaviour
+{
+    public Camera m_Camera;
+	
+	void Start(){
+		m_Camera = GameObject.Find("Main Camera").GetComponent<Camera>();
+	}
+ 
+    void Update()
+    {
+        transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
+            m_Camera.transform.rotation * Vector3.up);
+    }
+}
