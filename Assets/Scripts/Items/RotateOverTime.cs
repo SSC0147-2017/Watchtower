99d9d9d575b8c016875b0c7e099337fcc31d855e
jvguima <jvguima@yahoo.com.br>
2017-12-14 14:58:47 -0200

    Added Extras Model + Fix floofy anicon
    +RotateOverTime rotates a gameobject x degrees

---
+++ Assets/Scripts/Items/RotateOverTime.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateOverTime : MonoBehaviour {
+
+	public float angle;
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate(new Vector3(0,angle,0)*Time.deltaTime, Space.World);
+	}
+}
