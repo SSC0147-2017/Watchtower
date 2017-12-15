eb7c1caaa9b7b021cbecb5a8b10d09895dd025e5
Fabricio <fabricio-gf@hotmail.com>
2017-12-15 00:40:45 -0200

    Added instructions scene

---
+++ Assets/Scripts/Instructions/ImageSwitch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageSwitch : MonoBehaviour {
+
+	public Image imgA;
+	public Image imgB;
+
+	public float timeToChange = 5.0f;
+	public bool change = false;
+
+	// Use this for initialization
+	void Start () {
+		if (imgA == null)
+			imgA = GetComponent<Image> ();
+		if (imgB == null)
+			imgB = GetComponent<Image> ();	
+		StartCoroutine (timer ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (change) {
+			imgA.enabled = !imgA.enabled;
+			imgB.enabled = !imgB.enabled;
+			change = false;
+			StartCoroutine (timer ());
+		}
+	}
+
+	IEnumerator timer(){
+		yield return new WaitForSeconds (timeToChange);
+		change = true;
+	}
+}
---
+++ Assets/Scripts/Instructions/healthDecLoop.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthDecLoop : MonoBehaviour {
+
+	public Image img;
+	public float rate = 0.1f;
+	// Use this for initialization
+	void Start () {
+		if (img == null)
+			img = GetComponent<Image> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (img.fillAmount == 0)
+			img.fillAmount = 1;
+		else {
+			img.fillAmount -= (rate * Time.deltaTime);
+		}
+	}
+}
